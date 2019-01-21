7d0188cf6d22efdfccefde567b8097872dde37ac
Гапеев Дмитрий Игоревич <Dmitriy.Gapeev@galaktika.by>
2019-01-21 10:23:24 +0300

    Add project files.

---
+++ HashTableProject/HashTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsDataStructures
+{
+
+	public class HashTable
+	{
+		public int size;
+		public int step;
+		public string[] slots;
+
+		public HashTable(int sz, int stp)
+		{
+			size = sz;
+			step = stp;
+			slots = new string[size];
+			for (int i = 0; i < size; i++) slots[i] = null;
+		}
+
+		public int HashFun(string value)
+		{
+			int sumBytes = 0;
+			byte[] valuesBytes = Encoding.UTF8.GetBytes(value);
+
+			foreach (byte item in valuesBytes)
+				sumBytes += item;
+
+			return sumBytes % slots.Length;
+		}
+
+		public int SeekSlot(string value)
+		{
+			int position = HashFun(value);
+			for (int i = 0; i < slots.Length; i++)
+			{
+				if (slots[position] == null)
+					return position;
+
+				position += step;
+				position %= slots.Length;
+			}
+
+			return -1;
+		}
+
+		public int Put(string value)
+		{
+			int position = SeekSlot(value);
+			if (position != -1)
+				slots[position] = value;
+
+			return position;
+		}
+
+		public int Find(string value)
+		{
+			int position = HashFun(value);
+			for (int i = 0; i < slots.Length; i++)
+			{
+				if (slots[position] == value)
+					return position;
+
+				position += step;
+				position %= slots.Length;
+			}
+			return -1;
+		}
+	}
+
+}
---
+++ HashTableProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestHashTable();
+			Console.ReadKey();
+		}
+
+		static void TestHashTable()
+		{
+			Console.WriteLine("test hash table");
+			HashTable hashTable = new HashTable(11, 2);
+			char s = 'a';
+			for(int i = 0; i < 10; i++, s++)
+				hashTable.Put(new string(s, 10));
+			
+			foreach (string item in hashTable.slots)
+				Console.WriteLine(item + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("put z string");
+			hashTable.Put(new string('z', 10));
+
+			foreach (string item in hashTable.slots)
+				Console.WriteLine(item + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("try to put 'end'");
+			int position = hashTable.Put("end");
+			Console.WriteLine("result position = " + position);
+
+			Console.WriteLine("find values test");
+			s = 'a';
+			for (int i = 0; i < 10; i++, s++)
+			{
+				string value = new string(s, 10);
+				position = hashTable.Find(value);
+				Console.WriteLine("position = " + position + ", value = "+value);
+			}
+				
+
+			Console.WriteLine(new string('=', 50));
+		}
+	}
+}
